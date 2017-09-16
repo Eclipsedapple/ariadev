3f48c52eb27702c2197f8ea11313ea1e31cb599c
skyleroneill <32002100+skyleroneill@users.noreply.github.com>
2017-09-16 15:14:52 -0400

    Add files via upload

---
+++ Skyler_aria/Assets/Scripts/Gust.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gust : MonoBehaviour 
+{
+	public const int damage = 10;
+
+	void OnCollisionEnter(Collision col)
+	{
+		GameObject hit = col.gameObject;
+
+		if (CompareTag ("Enemy")) 
+		{
+			Debug.Log ("HIT!");
+		}
+
+	}
+}
